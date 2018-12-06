18d3650bb6c54828d96d90cc2d7e91cbf76233a8
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2018-12-07 11:26:02 +1300

    Moving files to public directory

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react'
 import Project from './project';
 
 class Projects extends Component {
+
+    state = {
+        projects: []
+    }
+
+    componentDidMount = () => {
+        this.loadProjects();
+    }
+
+    loadProjects = async () => {
+        const response = await fetch('/data/projects.json');
+        const projects = await response.json();
+
+        if(projects.length)
+        {
+            this.setState({
+                projects: projects
+            })
+        }
+    }
+
     render() {
-        if(!this.props.projects) return null;
+        if(!this.state.projects) return null;
         return (
             <div className="projects">
                 {
-                    this.props.projects.map((project, index) => {
+                    this.state.projects.map((project, index) => {
                         return <Project key={index} project={project} />
                     })
                 }
---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Projects from '../components/projects/projects'
+
+const ProjectPage = () => (
+    <Layout>
+        <Projects />
+    </Layout>
+)
+
+export default ProjectPage
